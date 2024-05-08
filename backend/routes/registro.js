d997a1f2f190c9e5bc8a002ee31fa9f790fa3f04
Lorena Aylen <lag9512@gmail.com>
2024-05-08 00:31:39 -0300

    Mejora: Modularización de lógica de validación
    Se separó la lógica para la validación de los forms en un middleware separado

--- backend/routes/registro.js
+++ backend/routes/registro.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const { checkSchema, validationResult} = require('express-validator')
 const validaciones = require('../utils/validacionesRegistro')
 const controller = require('../controllers/registroController')
+const funcionesComunes = require('../utils/funcionesComunes')
 
-router.post('/', checkSchema(validaciones), controller.registro )
+router.post('/', checkSchema(validaciones), funcionesComunes.validarJSON, controller.registro )
 
 module.exports = router
